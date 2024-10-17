4f1316038f9648ad0193d6e871bd87edbd97a10f
Muhammad Kevin Hardiansyah <141954008+kevinhardiansites@users.noreply.github.com>
2024-10-17 20:54:06 +0700

    Update app.py gemini flash

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
-load_dotenv()  # loading all the env variables
 from PIL import Image
 
+load_dotenv()  # loading all the env variables
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 def get_gemini_response(input_prompt, image):
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel('gemini-1.5-flash')  # Menggunakan model gemini-1.5-flash
     try:
         response = model.generate_content([input_prompt, image[0]])
         return response.text
@@ -41,16 +41,16 @@ if uploaded_file is not None:
 submit = st.button("Tell me the total calories")
 
 input_prompt = """
-You are an expert in nutritionist where you need to see the food items from the image
-and calculate the total calories, also provide the details of every food items with calories intake
-               is below format
+You are an expert in nutrition where you need to see the food items from the image
+and calculate the total calories. Also, provide the details of every food item with calorie intake
+in the following format:
 
-               1. Item 1 - no of calories
-               2. Item 2 - no of calories
-               ----
-               ----
-        Finally you can also mention whether the food is healthy or not and also
-        mention the percentage split of the ratio of carbohydrates, fats, fibers, sugar, protein, oils and other required in our diet
+1. Item 1 - no of calories
+2. Item 2 - no of calories
+----
+----
+Finally, you can also mention whether the food is healthy or not and also
+mention the percentage split of the ratio of carbohydrates, fats, fibers, sugar, protein, oils, and other required in our diet.
 """
 
 if submit:
